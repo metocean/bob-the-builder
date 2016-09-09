5e5de1e6bbe868162739ba8c314559c4ae2328bc
gregchalmers <g.chalmers@metocean.co.nz>
2016-09-09 13:13:54 +1200

    fixing setup

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 
 setup(
-    name='bob_the_builder',
+    name='bob',
     version='0.0.1',
     description='A Docker Compose building tool',
     author='Greg Chalmers',
     author_email='ops@metocean.co.nz',
 
-    packages=['bob_the_builder'],
+    packages=['bob'],
 
-    package_data={'bob_the_builder':
+    package_data={'bob':
                   ['static/css/*.css',
                    'static/fonts/*.*',
                    'static/images/*.*',
