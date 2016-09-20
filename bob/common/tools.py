3c8f4bf22b9ccbcbf70b36ddc3ec8386a94bcc50
gregchalmers <g.chalmers@metocean.co.nz>
2016-09-21 11:04:19 +1200

    added emailing into worker

---
+++ bob/common/tools.py
@@ -0,0 +1,18 @@
+import errno
+import os
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def mkdir_if_not_exist(path):
+    if not os.path.exists(path):
+        mkdir_p(path)
+    return path
