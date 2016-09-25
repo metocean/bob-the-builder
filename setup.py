e5d374eb0d81732bbf35e5ea4c84e85868f70187
gregchalmers <g.chalmers@metocean.co.nz>
2016-09-24 23:35:01 -0700

    for got to inc version again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bob',
-    version='0.2.1',
+    version='0.2.2',
     description='A Docker Compose building tool',
     author='Greg Chalmers',
     author_email='ops@metocean.co.nz',
