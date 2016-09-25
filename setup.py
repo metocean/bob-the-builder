a05a7cdad3c09a0964c82358e7ed6b50bf8ecaa0
gregchalmers <g.chalmers@metocean.co.nz>
2016-09-24 20:43:17 -0700

    forgot to increment verions num

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bob',
-    version='0.1.1',
+    version='0.2.0',
     description='A Docker Compose building tool',
     author='Greg Chalmers',
     author_email='ops@metocean.co.nz',
